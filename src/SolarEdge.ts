4d0fdfc962eba374663e0bbf1da36f7e20868259
Eric Jansen <ericjansen@live.nl>
2019-12-21 12:16:17 +0100

    fix eslint

--- src/SolarEdge.ts
+++ src/SolarEdge.ts
@@ -3,41 +3,45 @@ import fetch from 'node-fetch';
 type Period = 'DAY' | 'QUARTER_OF_AN_HOUR' | 'MONTH' | 'HOUR' | 'WEEK' | 'MONTH' | 'YEAR';
 
 export default class SolarEdge {
-  public HOST: string = 'https://monitoringapi.solaredge.com';
+    public HOST = 'https://monitoringapi.solaredge.com';
 
-  constructor(private API_KEY: string) {}
+    constructor(private API_KEY: string) {}
 
-  public fetchSolarEdge = async (url: string, options?: {}) => {
-    if (!url.toLowerCase().startsWith('http')) {
-      url = this.HOST + url;
-    }
-    const response = await fetch(url, options);
-    if (response.status !== 200) {
-      throw new Error(
-        'Error fetching SolarEdge data from URL ' + url + ': ' + response.status + ' - ' + response.statusText,
-      );
-    }
-    const data = await response.json();
-    return data;
-  };
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+    public fetchSolarEdge = async (url: string, options?: {}): any => {
+        if (!url.toLowerCase().startsWith('http')) {
+            url = this.HOST + url;
+        }
+        const response = await fetch(url, options);
+        if (response.status !== 200) {
+            throw new Error(
+                'Error fetching SolarEdge data from URL ' + url + ': ' + response.status + ' - ' + response.statusText,
+            );
+        }
+        const data = await response.json();
+        return data;
+    };
 
-  public getData = async (site: number, start: string, end: string, period: Period) =>
-    this.fetchSolarEdge(
-      this.HOST +
-        '/site/' +
-        site +
-        '/energy?timeUnit=' +
-        period.toUpperCase() +
-        '&endDate=' +
-        end +
-        '&startDate=' +
-        start +
-        '&api_key=' +
-        this.API_KEY,
-    );
+    // eslint-disable-next-line
+    public getData = async (site: number, start: string, end: string, period: Period) =>
+        this.fetchSolarEdge(
+            this.HOST +
+                '/site/' +
+                site +
+                '/energy?timeUnit=' +
+                period.toUpperCase() +
+                '&endDate=' +
+                end +
+                '&startDate=' +
+                start +
+                '&api_key=' +
+                this.API_KEY,
+        );
 
-  public getSiteData = async () => this.fetchSolarEdge(this.HOST + '/sites/list?size=1&api_key=' + this.API_KEY);
+    // eslint-disable-next-line
+    public getSiteData = async () => this.fetchSolarEdge(this.HOST + '/sites/list?size=1&api_key=' + this.API_KEY);
 
-  public getEquipmentData = async (site: number) =>
-    this.fetchSolarEdge(this.HOST + '/equipment/' + site + '/list?size=1&api_key=' + this.API_KEY);
+    // eslint-disable-next-line
+    public getEquipmentData = async (site: number) =>
+        this.fetchSolarEdge(this.HOST + '/equipment/' + site + '/list?size=1&api_key=' + this.API_KEY);
 }
