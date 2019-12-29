2b073a81b432f5cac2706023a56a468857e71346
Eric Jansen <ericjansen@live.nl>
2019-12-29 10:11:00 +0100

    delete solaredge file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
-export { default } from './SolarEdge';
+import axios, { AxiosRequestConfig } from 'axios';
 
-export class C {
-    private x = 10;
-    public getX = (): number => this.x;
-    public setX = (newVal: number): void => {
-        this.x = newVal;
+export type Period = 'DAY' | 'QUARTER_OF_AN_HOUR' | 'MONTH' | 'HOUR' | 'WEEK' | 'MONTH' | 'YEAR';
+
+export default class SolarEdge {
+    public HOST = 'https://monitoringapi.solaredge.com';
+
+    constructor(private API_KEY: string) {}
+
+    // eslint-disable-next-line
+    public fetchSolarEdge = async (url: string, options?: AxiosRequestConfig) => {
+        if (!url.toLowerCase().startsWith('http')) {
+            url = this.HOST + url;
+        }
+        const response = await axios.get(url, options);
+        if (response.status !== 200) {
+            throw new Error(
+                'Error fetching SolarEdge data from URL ' + url + ': ' + response.status + ' - ' + response.statusText,
+            );
+        }
+        return response.data;
     };
-}
 
-export const x = new C();
-export const y = { ...{ some: 'value' } };
+    // eslint-disable-next-line
+    public getData = async (site: number, start: string, end: string, period: Period) =>
+        this.fetchSolarEdge(
+            this.HOST +
+                '/site/' +
+                site +
+                '/energy?timeUnit=' +
+                period.toUpperCase() +
+                '&endDate=' +
+                end +
+                '&startDate=' +
+                start +
+                '&api_key=' +
+                this.API_KEY,
+        );
 
-export function sum(a: number, b: number): number {
-    return a + b;
+    // eslint-disable-next-line
+    public getSiteData = async () => this.fetchSolarEdge(this.HOST + '/sites/list?size=1&api_key=' + this.API_KEY);
+
+    // eslint-disable-next-line
+    public getEquipmentData = async (site: number) =>
+        this.fetchSolarEdge(this.HOST + '/equipment/' + site + '/list?size=1&api_key=' + this.API_KEY);
 }
